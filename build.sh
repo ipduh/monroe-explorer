e394b120e1f05b4e14b22dd1fbc9b412303dc22e
George Paitaris <gpa@incelligent.net>
2016-09-20 00:47:59 +0300

    .

--- build.sh
+++ build.sh
@@ -63,9 +63,8 @@ VIM='install'
 RUN_DOCKER_TRAFFIC_COUNT='yes'
 
 
-#ENTRYPOINT='["dumb-init", "--", "/usr/bin/perl", "/opt/monroe/monroe-explorer/monroe-explorer.pl"]'
+ENTRYPOINT='["dumb-init", "--", "/usr/bin/perl", "/opt/monroe/monroe-explorer/monroe-explorer.pl"]'
 #ENTRYPOINT='["dumb-init", "--", "/usr/bin/perl", "/opt/monroe/test/metadata-collector.pl"]'
-ENTRYPOINT='["dumb-init", "--", "/usr/bin/perl", "/opt/monroe/test/metadata-collector.pl 1800"]'
 
 #
 #CONFIG IS DONE
