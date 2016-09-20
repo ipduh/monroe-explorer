c8e2170e56328bca56e5d5108a712778fc70077d
George Paitaris <gpa@incelligent.net>
2016-09-20 17:01:14 +0300

    description

--- test/0mq-tinysrv.pl
+++ test/0mq-tinysrv.pl
@@ -2,6 +2,27 @@
 # 0MQ hello-server
 # g0, 2016
 
+
+=head1 Description
+  A tiny 0MQ REP server.
+  Motivation: Test if Perl-ZMQ-FFI works OK in the docker container.
+
+=cut
+
+=head1 Usage
+  Edit CONFIG stanza in source and run without arguments.
+  e.g. $ 0mq-tinysrv.pl
+  or
+  Pass IP_address and Port_number in the command line
+  e.g. $ 0mq-tinysrv.pl 172.17.0.1 5555
+
+=cut
+
+=head1 Author
+  g0, 2016, github@bot.ipduh.com
+
+=cut
+
 use strict;
 #use warnings;
 use v5.10;
@@ -9,8 +30,11 @@ use v5.10;
 use ZMQ::FFI;
 use ZMQ::FFI::Constants qw(ZMQ_REP);
 
+# CONFIG
 my $ip = '*';
 my $port = '5555';
+# CONFIG IS DONE
+
 
 if(scalar(@ARGV) > 0){
   if($ARGV[0] =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ && $1<256 && $2<256 && $3<256 && $4<256){
